0ed620a3844e042524e84f1754c6f142d64fcd36
Ľubomír Carik <Lubomir.Carik@gmail.com>
2021-03-28 23:37:55 +0200

    Deploy openconnect deplendencies into output folder
    This solve issue on Windows where dependencies was not in path
    and app refuse to start.

--- CMake/Includes/ProjectExternals_openconnect.cmake
+++ CMake/Includes/ProjectExternals_openconnect.cmake
@@ -52,7 +52,7 @@ endif()
 
 ExternalProject_Add(openconnect-${openconnect-TAG}
     PREFIX ${CMAKE_BINARY_DIR}/external
-    INSTALL_DIR ${CMAKE_BINARY_DIR}/external
+    INSTALL_DIR ${CMAKE_BINARY_DIR}/bin
     
     DOWNLOAD_NO_PROGRESS 1
 
@@ -63,52 +63,52 @@ ExternalProject_Add(openconnect-${openconnect-TAG}
     INSTALL_COMMAND ""
 )
 ExternalProject_Add_Step(openconnect-${openconnect-TAG} deploy_libs
-    COMMAND ${CMAKE_COMMAND} -E copy_directory . <INSTALL_DIR>/lib
+    COMMAND ${CMAKE_COMMAND} -E copy_directory . <INSTALL_DIR>
     COMMENT "...deploing openconnect-${openconnect-TAG} libraries"
     WORKING_DIRECTORY <SOURCE_DIR>
     DEPENDEES install
 )
 
 add_executable(openconnect::app IMPORTED)
-set_property(TARGET openconnect::app PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/openconnect.exe)
+set_property(TARGET openconnect::app PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/openconnect.exe)
 
 add_library(openconnect::gmp SHARED IMPORTED)
-set_property(TARGET openconnect::gmp PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libgmp-10.dll)
+set_property(TARGET openconnect::gmp PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libgmp-10.dll)
 set_property(TARGET openconnect::gmp PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libgmp.dll.a)
 
 add_library(openconnect::gnutls SHARED IMPORTED)
-set_property(TARGET openconnect::gnutls PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libgnutls-30.dll)
+set_property(TARGET openconnect::gnutls PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libgnutls-30.dll)
 set_property(TARGET openconnect::gnutls PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libgnutls.dll.a)
 
 add_library(openconnect::hogweed SHARED IMPORTED)
-set_property(TARGET openconnect::hogweed PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libhogweed-4.dll)
+set_property(TARGET openconnect::hogweed PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libhogweed-4.dll)
 set_property(TARGET openconnect::hogweed PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libhogweed.dll.a)
 
 add_library(openconnect::nettle SHARED IMPORTED)
-set_property(TARGET openconnect::nettle PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libnettle-6.dll)
+set_property(TARGET openconnect::nettle PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libnettle-6.dll)
 set_property(TARGET openconnect::nettle PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libnettle.dll.a)
 
 add_library(openconnect::openconnect SHARED IMPORTED)
-set_property(TARGET openconnect::openconnect PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libopenconnect-5.dll)
+set_property(TARGET openconnect::openconnect PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libopenconnect-5.dll)
 set_property(TARGET openconnect::openconnect PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libopenconnect.dll.a)
 
 add_library(openconnect::p11-kit SHARED IMPORTED)
-set_property(TARGET openconnect::p11-kit PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libp11-kit-0.dll)
+set_property(TARGET openconnect::p11-kit PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libp11-kit-0.dll)
 set_property(TARGET openconnect::p11-kit PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libp11-kit.dll.a)
 
 add_library(openconnect::stoken SHARED IMPORTED)
-set_property(TARGET openconnect::stoken PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libstoken-1.dll)
+set_property(TARGET openconnect::stoken PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libstoken-1.dll)
 set_property(TARGET openconnect::stoken PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libstoken.dll.a)
 
 add_library(openconnect::xml2 SHARED IMPORTED)
-set_property(TARGET openconnect::xml2 PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/external/lib/libxml2-2.dll)
+set_property(TARGET openconnect::xml2 PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/bin/libxml2-2.dll)
 set_property(TARGET openconnect::xml2 PROPERTY IMPORTED_IMPLIB ${CMAKE_BINARY_DIR}/external/lib/libxml2.dll.a)
 
 
 install(
     FILES
-        ${CMAKE_BINARY_DIR}/external/lib/openconnect.exe
-        ${CMAKE_BINARY_DIR}/external/lib/vpnc-script-win.js
+        ${CMAKE_BINARY_DIR}/bin/openconnect.exe
+        ${CMAKE_BINARY_DIR}/bin/vpnc-script-win.js
     DESTINATION .
     COMPONENT App_Console
 )
