b965da04b1de3ab1aa82ddfcb2e37c2f3534b111
Ľubomír Carik <Lubomir.Carik@gmail.com>
2021-03-28 23:49:13 +0200

    Bunp openconnect to v8.10

--- CMake/Includes/ProjectExternals.cmake
+++ CMake/Includes/ProjectExternals.cmake
@@ -1,6 +1,6 @@
 if(MINGW)
-    set(openconnect-TAG "v8.02" CACHE STRING "Please specify openconnect version")
-    set(openconnect-TAG_CHOICES "v8.02" "v7.08" "master")
+    set(openconnect-TAG "v8.10" CACHE STRING "Please specify openconnect version")
+    set(openconnect-TAG_CHOICES "v8.10" "v7.08" "master")
     set_property(CACHE openconnect-TAG PROPERTY STRINGS ${openconnect-TAG_CHOICES})
     if(NOT openconnect-TAG IN_LIST openconnect-TAG_CHOICES)
         message(FATAL_ERROR "Specify 'openconnect-TAG'. Must be one of ${openconnect-TAG_CHOICES}")
