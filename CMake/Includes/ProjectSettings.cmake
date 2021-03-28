31e535b25dc862b1a23072086b0e5d6caa933f52
Ľubomír Carik <Lubomir.Carik@gmail.com>
2021-03-28 23:24:41 +0200

    Small potential compilation speedup in UNIX based environments

--- CMake/Includes/ProjectSettings.cmake
+++ CMake/Includes/ProjectSettings.cmake
@@ -18,6 +18,7 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 
+add_compile_options("-pipe")
 add_compile_options("-Wall")
 add_compile_options("-Wextra")
 add_compile_options("-Wpedantic")
